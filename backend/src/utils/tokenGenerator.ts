be9edbc5f112002e1a68188e331a513be4c8bce7
Truong <68948864+vantruongno99@users.noreply.github.com>
2024-10-20 20:13:20 +1100

    Delete backend directory

--- backend/src/utils/tokenGenerator.ts
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { User } from '../models/user.modal';
-
-const tokenGenerator = (user: Partial<User>): string =>
-  jwt.sign(user, process.env.JWT_SECRET || 'superSecret', { expiresIn: '60d' });
-
-export default tokenGenerator;
